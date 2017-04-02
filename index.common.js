4b8752077a3cc2a7ab3592699959bb8ec70abfbf
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-04-02 23:26:44 +0530

    Update example

--- index.common.js
+++ index.common.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  BlobManager,
   ScrollView,
+  Button,
   View,
+  Image,
   Text,
-  TouchableOpacity,
   StyleSheet,
 } from 'react-native';
 import * as firebase from 'firebase';
 
-const uri = 'file:///storage/emulated/0/Download/content.png';
+global.BlobManager = BlobManager;
+
+const uri = 'file:///storage/emulated/0/Download/69555877.jpg';
+const url = 'https://firebasestorage.googleapis.com/v0/b/blobtest-36ff6.appspot.com/o/Obsidian.jar?alt=media&token=93154b97-8bd9-46e3-a51f-67be47a4628a';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAlZruO2T_JNOWn4ysfX6AryR6Dzm_VVaA',
@@ -25,7 +30,7 @@ const socket = new WebSocket('ws://localhost:7273');
 
 socket.onopen = async () => {
   console.log('Socket connected');
-  const file = await File.fromURI('file:///storage/emulated/0/Download/content.png', { type: 'image/png' });
+  const file = await BlobManager.createFromURI(uri, { type: 'image/png' });
   socket.send(file);
 };
 
@@ -37,7 +42,6 @@ class FirebaseReactNativeSample extends Component {
   };
 
   _xhrDownload = async () => {
-    const url = 'https://firebasestorage.googleapis.com/v0/b/blobtest-36ff6.appspot.com/o/psyduck.jpg?alt=media&token=9fde6e14-c5d1-4db8-aafc-4228b842f367';
     const req = new XMLHttpRequest();
     req.open('GET', url, true);
     req.responseType = 'blob';
@@ -54,6 +58,17 @@ class FirebaseReactNativeSample extends Component {
     req.send();
   }
 
+  _fetchDownload = async () => {
+    this._log('Downloading:', url);
+    try {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      this._log('Download success:', blob);
+    } catch (e) {
+      this._log('Download error:', e.message);
+    }
+  }
+
   _uploadToFirebase = async () => {
     this._log('Uploading to firebase:', this.state.blob);
     const ref = firebase.storage().ref().child('pokemon.jpg');
@@ -63,19 +78,10 @@ class FirebaseReactNativeSample extends Component {
 
   _createBlob = async () => {
     this._log('Creating Blob from:', uri);
-    const blob = await Blob.fromURI(uri, { type: 'image/png' });
+    const blob = await BlobManager.createFromURI(uri, { type: 'image/png' });
     this._log('File created:', blob);
     this.setState({
       blob,
-    });
-  };
-
-  _createFile = async () => {
-    this._log('Creating File from:', uri);
-    const file = await File.fromURI(uri, { type: 'image/png' });
-    this._log('File created:', file);
-    this.setState({
-      blob: file,
     });
   };
 
@@ -85,43 +91,44 @@ class FirebaseReactNativeSample extends Component {
       logs: state.logs.concat({
         message,
         data,
+        error: data && data instanceof Error,
       }),
     }));
   };
 
   _renderButton = (label, onPress) => (
-    <TouchableOpacity
-      onPress={async () => {
-        try {
-          await onPress();
-        } catch (e) {
-          this._log('Error', e);
-        }
-      }}
-    >
-      <Text style={styles.button}>
-        {label}
-      </Text>
-    </TouchableOpacity>
+    <View style={styles.button}>
+      <Button
+        onPress={async () => {
+          try {
+            await onPress();
+          } catch (e) {
+            this._log('Error', e);
+          }
+        }}
+        title={label}
+      />
+    </View>
   );
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <ScrollView style={styles.scroll} contentContainerStyle={styles.console}>
           {this.state.logs.map((item, i) => {
             const data = typeof item.data !== 'undefined' ? ' ' + JSON.stringify(item.data) : '';
             return (
               <Text key={i} style={styles.log}>
                 {item.message}
-                {data ? <Text style={styles.data}>{data}</Text> : null}
+                {data ? <Text style={[ styles.data, item.error && styles.error ]}>{data}</Text> : null}
               </Text>
             );
           })}
         </ScrollView>
+        {this.state.blob ? <Image style={styles.image} source={{ uri: URL.createObjectURL(this.state.blob) }} /> : null}
         {this._renderButton('Create blob from URI', this._createBlob)}
-        {this._renderButton('Create file from URI', this._createFile)}
-        {this._renderButton('Download binary file', this._xhrDownload)}
+        {this._renderButton('Download binary (XMLHttpRequest)', this._xhrDownload)}
+        {this._renderButton('Download binary (Fetch)', this._fetchDownload)}
         {this._renderButton('Upload to firebase', this._uploadToFirebase)}
       </View>
     );
@@ -129,15 +136,25 @@ class FirebaseReactNativeSample extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+  },
+
   console: {
     padding: 12,
     transform: [ { scaleY: -1 } ],
   },
 
+  image: {
+    height: 100,
+    width: 100,
+  },
+
   scroll: {
+    flex: 1,
     backgroundColor: '#212733',
-    margin: 16,
-    height: 320,
+    margin: 8,
     transform: [ { scaleY: -1 } ],
   },
 
@@ -149,13 +166,14 @@ const styles = StyleSheet.create({
 
   button: {
     margin: 8,
-    fontSize: 16,
-    textAlign: 'center',
-    color: '#2196F3',
   },
 
   data: {
     color: '#00BCD4',
+  },
+
+  error: {
+    color: '#FF9800',
   },
 });
 
